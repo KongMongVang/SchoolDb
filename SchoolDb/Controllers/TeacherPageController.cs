a5da3afe21b058a31595556f6ee22baf4868429e
Kong Mong Vang <vangkong@icloud.com>
2025-07-18 21:22:25 -0400

    assignment complete

--- SchoolDb/Controllers/TeacherPageController.cs
+++ SchoolDb/Controllers/TeacherPageController.cs
@@ -8,18 +8,28 @@ namespace SchoolDb.Controllers
     public class TeacherPageController : Controller
     {
         private TeacherAPIController _api;
+        // This is a Dependency Injection
         public TeacherPageController(TeacherAPIController api)
         {
             _api = api;
         }
 
         [HttpGet]
+        // This will return a list of first name and last name of all teachers
         public IActionResult List()
         {
-        List<Teacher> Teachers = _api.GetTeacherInfo();
-    
+            List<Teacher> Teachers = _api.GetTeacherInfo();
+
 
             return View(Teachers);
+        }
+        [HttpGet]
+        // This will search the table with the teacher id and return the all the details of that teacher
+        public IActionResult Show(int id)
+        {
+            Teacher TeacherDetails = _api.TeacherInfo(id);
+
+            return View(TeacherDetails);
         }
     }
 }
